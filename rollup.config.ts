615109d1198c3499e9a03f3afd5061c50ec0c5a7
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2021-12-27 06:58:49 +0100

    fix: resolve lint error

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,6 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import Table from 'easy-table';
 import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
